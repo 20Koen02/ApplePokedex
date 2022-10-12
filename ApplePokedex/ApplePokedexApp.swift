d972ea1d324dd583507038627a63d28ba511acb2
Koen van Wijngaarden <mail@koen02.nl>
2022-10-12 15:51:06 +0200

    Initial Commit

---
+++ ApplePokedex/ApplePokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplePokedexApp.swift
+//  ApplePokedex
+//
+//  Created by Koen van Wijngaarden on 12/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ApplePokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
